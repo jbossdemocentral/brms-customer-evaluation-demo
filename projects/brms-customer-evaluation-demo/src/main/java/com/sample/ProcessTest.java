e9d087111757a8374080320609131d48a429d473
Eric D. Schabell <eric@schabell.org>
2012-06-11 11:29:16 +0200

    Added in initial demo project.

---
+++ projects/brms-customer-evaluation-demo/src/main/java/com/sample/ProcessTest.java
@@ -0,0 +1,22 @@
+package com.sample;
+
+import org.drools.runtime.StatefulKnowledgeSession;
+import org.drools.runtime.process.ProcessInstance;
+import org.jbpm.test.JbpmJUnitTestCase;
+import org.junit.Test;
+
+/**
+ * This is a sample file to test a process.
+ */
+public class ProcessTest extends JbpmJUnitTestCase {
+
+	@Test
+	public void testProcess() {
+		StatefulKnowledgeSession ksession = createKnowledgeSession("sample.bpmn");
+		ProcessInstance processInstance = ksession.startProcess("com.sample.bpmn.hello");
+		// check whether the process instance has completed successfully
+		assertProcessInstanceCompleted(processInstance.getId(), ksession);
+		assertNodeTriggered(processInstance.getId(), "StartProcess", "Hello", "EndProcess");
+	}
+
+}
