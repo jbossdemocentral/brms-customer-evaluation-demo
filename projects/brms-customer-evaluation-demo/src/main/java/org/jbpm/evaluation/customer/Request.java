62e927d053e391046a95a4ddd57cfb839971c2e5
Rafael Benevides <rafabene@gmail.com>
2013-07-25 16:48:13 -0300

    JDF-423 - Put BRMS quickstarts under JDF Guidelines

--- projects/brms-customer-evaluation-demo/src/main/java/org/jbpm/evaluation/customer/Request.java
+++ projects/brms-customer-evaluation-demo/src/main/java/org/jbpm/evaluation/customer/Request.java
@@ -1,81 +1,98 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jbpm.evaluation.customer;
 
 import java.io.Serializable;
 
 public class Request implements Serializable {
-	
-	/**
-	 * Default ID.
-	 */
-	private static final long serialVersionUID = 1L;
-	private String id;
-	private String personId;
-	private Integer amount;
-	private boolean valid;
-	private String invalidReason;
-	private boolean canceled;
 
-	public Request(String id) {
-		this.id = id;
-	}
-	
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getId() {
-		return id;
-	}
-	
-	public String getPersonId() {
-		return personId;
-	}
+    /**
+     * Default ID.
+     */
+    private static final long serialVersionUID = 1L;
+    private String id;
+    private String personId;
+    private Integer amount;
+    private boolean valid;
+    private String invalidReason;
+    private boolean canceled;
 
-	public void setPersonId(String personId) {
-		this.personId = personId;
-	}
-	
-	public Integer getAmount() {
-		return amount;
-	}
+    public Request(String id) {
+        this.id = id;
+    }
 
-	public void setAmount(Integer amount) {
-		this.amount = amount;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public void setInvalid(String reason) {
-		this.valid = false;
-		this.invalidReason = reason;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public boolean isValid() {
-		return valid;
-	}
-	
-	public void setValid() {
-		this.valid = true;
-	}
+    public String getPersonId() {
+        return personId;
+    }
 
-	public String getInvalidReason() {
-		return invalidReason;
-	}
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
 
-	public boolean isCanceled() {
-		return canceled;
-	}
+    public Integer getAmount() {
+        return amount;
+    }
 
-	public void setCanceled(boolean canceled) {
-		this.canceled = canceled;
-	}
-	
-	public boolean equals(Object o) {
-		if (o instanceof Request) {
-			return this.id.equals(((Request) o).id);
-		}
-		return false;
-	}
-	
-	public int hashCode() {
-		return this.id.hashCode();
-	}
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public void setInvalid(String reason) {
+        this.valid = false;
+        this.invalidReason = reason;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public void setValid() {
+        this.valid = true;
+    }
+
+    public String getInvalidReason() {
+        return invalidReason;
+    }
+
+    public boolean isCanceled() {
+        return canceled;
+    }
+
+    public void setCanceled(boolean canceled) {
+        this.canceled = canceled;
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof Request) {
+            return this.id.equals(((Request) o).id);
+        }
+        return false;
+    }
+
+    public int hashCode() {
+        return this.id.hashCode();
+    }
 
 }
--- projects/brms-customer-evaluation-demo/src/test/java/org/jbpm/evaluation/customer/CustomerEvaluationTest.java
+++ projects/brms-customer-evaluation-demo/src/test/java/org/jbpm/evaluation/customer/CustomerEvaluationTest.java
@@ -1,3 +1,19 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jbpm.evaluation.customer;
 
 import java.util.HashMap;
@@ -7,6 +23,8 @@ import org.drools.KnowledgeBase;
 import org.drools.builder.ResourceType;
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.process.WorkflowProcessInstance;
+import org.jbpm.evaluation.customer.Person;
+import org.jbpm.evaluation.customer.Request;
 import org.jbpm.test.JbpmJUnitTestCase;
 import org.junit.Test;
 
@@ -15,204 +33,205 @@ import org.junit.Test;
  */
 public class CustomerEvaluationTest extends JbpmJUnitTestCase {
 
-	private static Integer underAged    = 11;
-	private static Integer adultAged    = 25;
-	private static Integer richCustomer = 2000; // greater than 999.
-	private static Integer poorCutomer  = 2;
-	private static KnowledgeBase kbase; 
-	private static StatefulKnowledgeSession ksession;
-	
-	public CustomerEvaluationTest() {
-		super(true);
-	}
-	
-	private void setupTestCase() {
-		Map<String, ResourceType> kbtypes = new HashMap<String, ResourceType>();
-		kbtypes.put("financerules.drl", ResourceType.DRL);
-		kbtypes.put("customereval.bpmn2", ResourceType.BPMN2);
-		
-		kbase = null;
-		try {
-			kbase = createKnowledgeBase(kbtypes);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		ksession = createKnowledgeSession(kbase); 
-	}
-	@Test
-	public void underagedCustomerEvaluationTest() {
+    private static Integer underAged = 11;
+    private static Integer adultAged = 25;
+    private static Integer richCustomer = 2000; // greater than 999.
+    private static Integer poorCutomer = 2;
+    private static KnowledgeBase kbase;
+    private static StatefulKnowledgeSession ksession;
 
-		setupTestCase();
+    public CustomerEvaluationTest() {
+        super(true);
+    }
 
-		// optional: setup logging.
-		//KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newThreadedFileLogger(ksession, "CustomerEvaluationUnderaged", 1000);
-	
-		// setup of a Person and Request.
-		Person underagedEval = getUnderagedCustomer();
-		Request richEval = getRichCustomer();
-		ksession.insert(underagedEval);
+    private void setupTestCase() {
+        Map<String, ResourceType> kbtypes = new HashMap<String, ResourceType>();
+        kbtypes.put("financerules.drl", ResourceType.DRL);
+        kbtypes.put("customereval.bpmn2", ResourceType.BPMN2);
 
-		// Map to be passed to the startProcess.
-		Map<String, Object> params = new HashMap<String, Object>();
-		params.put("person", underagedEval);
-		params.put("request", richEval);
-		
-		// Fire it up!
-		System.out.println("=========================================");
-		System.out.println("= Starting Process Underaged Test Case. =");
-		System.out.println("=========================================");
+        kbase = null;
+        try {
+            kbase = createKnowledgeBase(kbtypes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ksession = createKnowledgeSession(kbase);
+    }
 
-		WorkflowProcessInstance processInstance = (WorkflowProcessInstance) ksession.startProcess("org.jbpm.customer-evaluation", params);
-		ksession.insert(processInstance);
-		ksession.fireAllRules();
-				
-		// Finished, clean up the logger.
-		assertProcessInstanceCompleted(processInstance.getId(), ksession);
-		assertNodeTriggered(processInstance.getId(), "Underaged");
-		//logger.close();
-		ksession.dispose();
-	}
+    @Test
+    public void underagedCustomerEvaluationTest() {
 
-	@Test
-	public void adultCustomerEvaluationTest() {
+        setupTestCase();
 
-		setupTestCase();
-		
-		// optional: setup logging.
-		//KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newThreadedFileLogger(ksession, "CustomerEvaluationPoorAdult", 1000);
-	
-		// setup of a Person and Request.
-		Person adultEval = getAdultCustomer();
-		Request poorEval = getPoorCustomer();
-		ksession.insert(adultEval);
+        // optional: setup logging.
+        // KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newThreadedFileLogger(ksession,
+        // "CustomerEvaluationUnderaged", 1000);
 
-		// Map to be passed to the startProcess.
-		Map<String, Object> params = new HashMap<String, Object>();
-		params.put("person", adultEval);
-		params.put("request", poorEval);
-		
-		// Fire it up!
-		System.out.println("==========================================");
-		System.out.println("= Starting Process Poor Adult Test Case. =");
-		System.out.println("==========================================");
+        // setup of a Person and Request.
+        Person underagedEval = getUnderagedCustomer();
+        Request richEval = getRichCustomer();
+        ksession.insert(underagedEval);
 
-		WorkflowProcessInstance processInstance = (WorkflowProcessInstance) ksession.startProcess("org.jbpm.customer-evaluation", params);
-		ksession.insert(processInstance);
-		ksession.fireAllRules();
-				
-		// Finished, clean up the logger.
-		assertProcessInstanceCompleted(processInstance.getId(), ksession);
-		assertNodeTriggered(processInstance.getId(), "End Poor Customer");
-		//logger.close();
-		ksession.dispose();
-	}
+        // Map to be passed to the startProcess.
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("person", underagedEval);
+        params.put("request", richEval);
 
-	@Test
-	public void richCustomerEvaluationTest() {
+        // Fire it up!
+        System.out.println("=========================================");
+        System.out.println("= Starting Process Underaged Test Case. =");
+        System.out.println("=========================================");
 
-		setupTestCase();
-		
-		// optional: setup logging.
-		//KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newThreadedFileLogger(ksession, "CustomerEvaluationRichAdult", 1000);
-	
-		// setup of a Person and Request.
-		Person adultEval = getAdultCustomer();
-		Request richEval = getRichCustomer();
-		ksession.insert(adultEval);
+        WorkflowProcessInstance processInstance = (WorkflowProcessInstance) ksession.startProcess("org.jbpm.customer-evaluation", params);
+        ksession.insert(processInstance);
+        ksession.fireAllRules();
 
-		// Map to be passed to the startProcess.
-		Map<String, Object> params = new HashMap<String, Object>();
-		params.put("person", adultEval);
-		params.put("request", richEval);
-		
-		// Fire it up!
-		System.out.println("==========================================");
-		System.out.println("= Starting Process Rich Adult Test Case. =");
-		System.out.println("==========================================");
-		WorkflowProcessInstance processInstance = (WorkflowProcessInstance) ksession.startProcess("org.jbpm.customer-evaluation", params);
-		ksession.insert(processInstance);
-		ksession.fireAllRules();
-				
-		// Finished, clean up the logger.
-		assertProcessInstanceCompleted(processInstance.getId(), ksession);
-		assertNodeTriggered(processInstance.getId(), "End Rich Customer");
-		//logger.close();
-		ksession.dispose();
-	}
+        // Finished, clean up the logger.
+        assertProcessInstanceCompleted(processInstance.getId(), ksession);
+        assertNodeTriggered(processInstance.getId(), "Underaged");
+        // logger.close();
+        ksession.dispose();
+    }
 
-	
-	@Test
-	public void emptyRequestCustomerEvaluationTest() {
+    @Test
+    public void adultCustomerEvaluationTest() {
 
-		setupTestCase();
-		
-		// optional: setup logging.
-		//KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newThreadedFileLogger(ksession, "CustomerEvaluationEmptyRequest", 1000);
-	
+        setupTestCase();
 
-		// Map to be passed to the startProcess is intentionally empty.
-		Map<String, Object> params = new HashMap<String, Object>();
-		
-		// Fire it up!
-		System.out.println("=============================================");
-		System.out.println("= Starting Process Empty Request Test Case. =");
-		System.out.println("=============================================");
-		WorkflowProcessInstance processInstance = (WorkflowProcessInstance) ksession.startProcess("org.jbpm.customer-evaluation", params);
-		ksession.insert(processInstance);
-		ksession.fireAllRules();
-				
-		// Finished, clean up the logger.
-		assertProcessInstanceCompleted(processInstance.getId(), ksession);
-		assertNodeTriggered(processInstance.getId(), "Underaged");
-		//logger.close();
-		ksession.dispose();
-	}
+        // optional: setup logging.
+        // KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newThreadedFileLogger(ksession,
+        // "CustomerEvaluationPoorAdult", 1000);
 
+        // setup of a Person and Request.
+        Person adultEval = getAdultCustomer();
+        Request poorEval = getPoorCustomer();
+        ksession.insert(adultEval);
 
-	/**
-	 * Provide an under aged person.
-	 * 
-	 * @return org.jbpm.evaluation.customer.Person
-	 */
-	private Person getUnderagedCustomer() {
-		Person person = new Person("erics", "Eric D. Schabell");
-		person.setAge(underAged);
-		return person;
-	}
+        // Map to be passed to the startProcess.
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("person", adultEval);
+        params.put("request", poorEval);
 
-	
-	/**
-	 * Provide an of aged person.
-	 * 
-	 * @return org.jbpm.evaluation.customer.Person
-	 */
-	private Person getAdultCustomer() {
-		Person person = new Person("erics", "Eric D. Schabell");
-		person.setAge(adultAged);
-		return person;
-	}
+        // Fire it up!
+        System.out.println("==========================================");
+        System.out.println("= Starting Process Poor Adult Test Case. =");
+        System.out.println("==========================================");
 
-	/**
-	 * Provide a poor person in the request.
-	 * 
-	 * @return org.jbpm.evaluation.customer.Request
-	 */
-	private Request getPoorCustomer() {
-		Request request = new Request("1");
-		request.setPersonId("erics");
-		request.setAmount(poorCutomer);
-		return request;
-	}
+        WorkflowProcessInstance processInstance = (WorkflowProcessInstance) ksession.startProcess("org.jbpm.customer-evaluation", params);
+        ksession.insert(processInstance);
+        ksession.fireAllRules();
 
-	/**
-	 * Provide a rich person in the request.
-	 * 
-	 * @return org.jbpm.evaluation.customer.Request
-	 */
-	private Request getRichCustomer() {
-		Request request = new Request("1");
-		request.setPersonId("erics");
-		request.setAmount(richCustomer);
-		return request;
-	}
+        // Finished, clean up the logger.
+        assertProcessInstanceCompleted(processInstance.getId(), ksession);
+        assertNodeTriggered(processInstance.getId(), "End Poor Customer");
+        // logger.close();
+        ksession.dispose();
+    }
+
+    @Test
+    public void richCustomerEvaluationTest() {
+
+        setupTestCase();
+
+        // optional: setup logging.
+        // KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newThreadedFileLogger(ksession,
+        // "CustomerEvaluationRichAdult", 1000);
+
+        // setup of a Person and Request.
+        Person adultEval = getAdultCustomer();
+        Request richEval = getRichCustomer();
+        ksession.insert(adultEval);
+
+        // Map to be passed to the startProcess.
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("person", adultEval);
+        params.put("request", richEval);
+
+        // Fire it up!
+        System.out.println("==========================================");
+        System.out.println("= Starting Process Rich Adult Test Case. =");
+        System.out.println("==========================================");
+        WorkflowProcessInstance processInstance = (WorkflowProcessInstance) ksession.startProcess("org.jbpm.customer-evaluation", params);
+        ksession.insert(processInstance);
+        ksession.fireAllRules();
+
+        // Finished, clean up the logger.
+        assertProcessInstanceCompleted(processInstance.getId(), ksession);
+        assertNodeTriggered(processInstance.getId(), "End Rich Customer");
+        // logger.close();
+        ksession.dispose();
+    }
+
+    @Test
+    public void emptyRequestCustomerEvaluationTest() {
+
+        setupTestCase();
+
+        // optional: setup logging.
+        // KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newThreadedFileLogger(ksession,
+        // "CustomerEvaluationEmptyRequest", 1000);
+
+        // Map to be passed to the startProcess is intentionally empty.
+        Map<String, Object> params = new HashMap<String, Object>();
+
+        // Fire it up!
+        System.out.println("=============================================");
+        System.out.println("= Starting Process Empty Request Test Case. =");
+        System.out.println("=============================================");
+        WorkflowProcessInstance processInstance = (WorkflowProcessInstance) ksession.startProcess("org.jbpm.customer-evaluation", params);
+        ksession.insert(processInstance);
+        ksession.fireAllRules();
+
+        // Finished, clean up the logger.
+        assertProcessInstanceCompleted(processInstance.getId(), ksession);
+        assertNodeTriggered(processInstance.getId(), "Underaged");
+        // logger.close();
+        ksession.dispose();
+    }
+
+    /**
+     * Provide an under aged person.
+     * 
+     * @return org.jbpm.evaluation.customer.Person
+     */
+    private Person getUnderagedCustomer() {
+        Person person = new Person("erics", "Eric D. Schabell");
+        person.setAge(underAged);
+        return person;
+    }
+
+    /**
+     * Provide an of aged person.
+     * 
+     * @return org.jbpm.evaluation.customer.Person
+     */
+    private Person getAdultCustomer() {
+        Person person = new Person("erics", "Eric D. Schabell");
+        person.setAge(adultAged);
+        return person;
+    }
+
+    /**
+     * Provide a poor person in the request.
+     * 
+     * @return org.jbpm.evaluation.customer.Request
+     */
+    private Request getPoorCustomer() {
+        Request request = new Request("1");
+        request.setPersonId("erics");
+        request.setAmount(poorCutomer);
+        return request;
+    }
+
+    /**
+     * Provide a rich person in the request.
+     * 
+     * @return org.jbpm.evaluation.customer.Request
+     */
+    private Request getRichCustomer() {
+        Request request = new Request("1");
+        request.setPersonId("erics");
+        request.setAmount(richCustomer);
+        return request;
+    }
 }
