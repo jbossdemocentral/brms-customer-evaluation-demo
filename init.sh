d666be85af22cb3bef74f69f123b10bf8dd0eccb
Eric D. Schabell <eric@schabell.org>
2013-09-23 20:11:29 +0200

    Just getting init script sorted out for eap 6.1.0 and bpm suite 6.0.0.Beta.

--- init.sh
+++ init.sh
@@ -7,11 +7,10 @@ SERVER_DIR=$JBOSS_HOME/standalone/deployments/
 SERVER_CONF=$JBOSS_HOME/standalone/configuration/
 SRC_DIR=./installs
 PRJ_DIR=./projects/brms-customer-evaluation-demo
-EAP=jboss-eap-6.1.1.zip
-BRMS=brms-p-5.3.1.GA-deployable-ee6.zip
-DESIGNER=designer-patched.war
-VERSION=5.3.1
-MAVENIZE_VERSION=5.3.1.BRMS
+EAP=jboss-eap-6.1.0.zip
+BRMS=jboss-bpms-6.0.0-redhat-3-deployable.zip
+VERSION=6.0.0.Beta
+#MAVENIZE_VERSION=5.3.1.BRMS
 
 # wipe screen.
 clear 
@@ -19,34 +18,34 @@ clear
 ##
 # Installation mavanization functions.
 ##
-installPom() {
-		mvn -q install:install-file -Dfile=../support/$2-$MAVENIZE_VERSION.pom.xml -DgroupId=$1 -DartifactId=$2 -Dversion=$MAVENIZE_VERSION -Dpackaging=pom;
-}
-
-	installBinary() {
-			unzip -q $2-$MAVENIZE_VERSION.jar META-INF/maven/$1/$2/pom.xml;
-			mvn -q install:install-file -DpomFile=./META-INF/maven/$1/$2/pom.xml -Dfile=$2-$MAVENIZE_VERSION.jar -DgroupId=$1 -DartifactId=$2 -Dversion=$MAVENIZE_VERSION -Dpackaging=jar;
-}
+#installPom() {
+#		mvn -q install:install-file -Dfile=../support/$2-$MAVENIZE_VERSION.pom.xml -DgroupId=$1 -DartifactId=$2 -Dversion=$MAVENIZE_VERSION -Dpackaging=pom;
+#}
+#
+#	installBinary() {
+#			unzip -q $2-$MAVENIZE_VERSION.jar META-INF/maven/$1/$2/pom.xml;
+#			mvn -q install:install-file -DpomFile=./META-INF/maven/$1/$2/pom.xml -Dfile=$2-$MAVENIZE_VERSION.jar -DgroupId=$1 -DartifactId=$2 -Dversion=$MAVENIZE_VERSION -Dpackaging=jar;
+#}
 
 echo
-echo "##################################################################"
-echo "##                                                              ##"   
-echo "##  Setting up the ${DEMO}                     ##"
-echo "##                                                              ##"   
-echo "##                                                              ##"   
-echo "##              ####   ####    #   #    ###                     ##"
-echo "##              #   #  #   #  # # # #  #                        ##"
-echo "##              ####   ####   #  #  #   ##                      ##"
-echo "##              #   #  #  #   #     #     #                     ##"
-echo "##              ####   #   #  #     #  ###                      ##"
-echo "##                                                              ##"   
-echo "##                                                              ##"   
-echo "##  brought to you by,                                          ##"   
-echo "##             ${AUTHORS}                                 ##"
-echo "##                                                              ##"   
-echo "##  ${PROJECT}  ##"
-echo "##                                                              ##"   
-echo "##################################################################"
+echo "#################################################################"
+echo "##                                                             ##"   
+echo "##  Setting up the ${DEMO}                    ##"
+echo "##                                                             ##"   
+echo "##                                                             ##"   
+echo "##     ####  ####   #   #      ### #   # ##### ##### #####     ##"
+echo "##     #   # #   # # # # #    #    #   #   #     #   #         ##"
+echo "##     ####  ####  #  #  #     ##  #   #   #     #   ###       ##"
+echo "##     #   # #     #     #       # #   #   #     #   #         ##"
+echo "##     ####  #     #     #    ###  ##### #####   #   #####     ##"
+echo "##                                                             ##"   
+echo "##                                                             ##"   
+echo "##  brought to you by,                                         ##"   
+echo "##             ${AUTHORS}                                ##"
+echo "##                                                             ##"   
+echo "##  ${PROJECT} ##"
+echo "##                                                             ##"   
+echo "#################################################################"
 echo
 
 command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not installed yet... aborting."; exit 1; }
@@ -92,6 +91,8 @@ else
 		unzip -q -d target $SRC_DIR/$EAP
 fi
 
+exit
+
 # Unzip the required files from JBoss BRMS Deployable
 echo Unpacking JBoss Enterprise BRMS $VERSION...
 echo
@@ -120,15 +121,6 @@ echo Rounding up, setting permissions and copying support files...
 echo
 cd ../
 
-echo Updating to the newest web designer...
-echo
-rm -rf $SERVER_DIR/designer.war/*
-unzip -q support/$DESIGNER -d $SERVER_DIR/designer.war
-
-echo "  - set designer to jboss-brms in profile..."
-echo
-cp support/designer-jbpm.xml $SERVER_DIR/designer.war/profiles/jbpm.xml
-
 echo "  - enabling demo accounts logins in brms-users.properties file..."
 echo
 cp support/brms-users.properties $SERVER_CONF
@@ -145,10 +137,6 @@ touch $SERVER_DIR/designer.war.dodeploy
 touch $SERVER_DIR/jboss-brms.war.dodeploy
 touch $SERVER_DIR/jbpm-human-task.war.dodeploy
 
-echo "  - configuring security authentication, copying updated components.xml file to jboss-brms.war..."
-echo
-cp support/components.xml $SERVER_DIR/jboss-brms.war/WEB-INF/
-
 echo "  - configuring deployment timeout extention and added security domain brms in standalone.xml..."
 echo
 cp support/standalone.xml $SERVER_CONF
@@ -157,102 +145,6 @@ cp support/standalone.xml $SERVER_CONF
 echo "  - making sure standalone.sh for server is executable..."
 echo
 chmod u+x $JBOSS_HOME/bin/standalone.sh
-
-echo "  - enabling demo users for human tasks in jbpm-human-task.war web.xml file..."
-echo
-cp support/jbpm-human-task-war-web.xml $SERVER_DIR/jbpm-human-task.war/WEB-INF/web.xml
-
-echo "  - enabling work items by registering Email and Log nodes..."
-echo
-cp support/drools.session.conf $SERVER_DIR/business-central-server.war/WEB-INF/classes/META-INF
-cp support/CustomWorkItemHandlers.conf $SERVER_DIR/business-central-server.war/WEB-INF/classes/META-INF
-chmod 644 $SERVER_DIR/business-central-server.war/WEB-INF/classes/META-INF/drools.session.conf
-chmod 644 $SERVER_DIR/business-central-server.war/WEB-INF/classes/META-INF/CustomWorkItemHandlers.conf
-
-#echo "  - adding model jar to business central admin console classpath..." 
-#echo
-#cp support/customereval-model.jar $SERVER_DIR/business-central-server.war/WEB-INF/lib
-
-echo "  - adding netty dep to business-central-server.war and jbpm-human-task.war..."
-echo
-cp support/MANIFEST.MF $SERVER_DIR/business-central-server.war/WEB-INF/classes/META-INF/
-cp support/MANIFEST.MF $SERVER_DIR/jbpm-human-task.war/WEB-INF/classes/META-INF/
-
-echo "  - mavenizing your repo with BRMS components."
-echo
-echo
-echo Installing the BRMS binaries into the Maven repository...
-echo
-unzip -q $SRC_DIR/$BRMS jboss-brms-engine.zip
-unzip -q jboss-brms-engine.zip binaries/*
-unzip -q $SRC_DIR/$BRMS jboss-jbpm-engine.zip
-unzip -q -o -d ./binaries jboss-jbpm-engine.zip
-cd binaries
-
-echo Installing parent POMs...
-echo
-installPom org.drools droolsjbpm-parent
-installPom org.drools droolsjbpm-knowledge
-installPom org.drools drools-multiproject
-installPom org.drools droolsjbpm-tools
-installPom org.drools droolsjbpm-integration
-installPom org.drools guvnor
-installPom org.jbpm jbpm
-
-echo Installing Rules dependencies into your Maven repository...
-echo
-#
-# droolsjbpm-knowledge
-installBinary org.drools knowledge-api
-
-#
-# drools-multiproject
-installBinary org.drools drools-core
-installBinary org.drools drools-compiler
-installBinary org.drools drools-jsr94
-installBinary org.drools drools-verifier
-installBinary org.drools drools-persistence-jpa
-installBinary org.drools drools-templates
-installBinary org.drools drools-decisiontables
-
-#
-# droolsjbpm-tools
-installBinary org.drools drools-ant
-
-#
-# droolsjbpm-integration
-installBinary org.drools drools-camel
-
-#
-# guvnor
-installBinary org.drools droolsjbpm-ide-common
-
-echo Installing BPM dependencies into your Maven repository...
-echo
-installBinary org.jbpm jbpm-flow
-installBinary org.jbpm jbpm-flow-builder
-installBinary org.jbpm jbpm-persistence-jpa
-installBinary org.jbpm jbpm-bam
-installBinary org.jbpm jbpm-bpmn2
-installBinary org.jbpm jbpm-workitems
-installBinary org.jbpm jbpm-human-task
-installBinary org.jbpm jbpm-test
-
-cd ..
-rm -rf binaries jboss-jbpm-engine.zip jboss-brms-engine.zip 
-
-echo Installation of binaries for BRMS $MAVENIZE_VERSION complete.
-echo
-
-echo Now going to build the model jars by generating classes in your project.
-echo
-cd $PRJ_DIR
-mvn clean install -DskipTests
-cd ../..
-
-echo "  - copying model jars and configuration to Business Central server..."
-echo 
-cp $PRJ_DIR/target/customereval-model.jar $SERVER_DIR/business-central-server.war/WEB-INF/lib
 
 echo "JBoss Enterprise BRMS ${VERSION} ${DEMO} Setup Complete."
 echo
